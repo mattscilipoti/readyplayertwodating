ea992a817ea425ac25d5de835769ca5d5ea37cc8
alea qawiyy <aq1984@gmail.com>
2015-11-16 12:13:07 -0500

    adjusted application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
