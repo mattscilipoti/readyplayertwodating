ecda71875413a9d3ffe638602a4e5167e2037bae
alea qawiyy <aq1984@gmail.com>
2015-11-16 11:21:05 -0500

    adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:password => "passwords"}
   root 'pages#welcome'
 
-match '/about' => 'pages#about'
-match '/help' => 'pages#help'
+get 'about' => 'pages#about'
+get 'help' => 'pages#help'
 
 resources :users
 resources :profiles
