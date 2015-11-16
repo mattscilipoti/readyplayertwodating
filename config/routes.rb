672c8be44f7ba3e1279a3badccb80cf1a69d0adf
alea qawiyy <aq1984@gmail.com>
2015-11-16 11:19:53 -0500

    added help and about routes views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:password => "passwords"}
   root 'pages#welcome'
 
-
+match '/about' => 'pages#about'
+match '/help' => 'pages#help'
 
 resources :users
 resources :profiles
